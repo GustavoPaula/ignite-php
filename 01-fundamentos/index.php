4ff476e0e6a277a4858d9ae855701973f10fd08a
GustavoPaula <gustavo.sdepaula09@gmail.com>
2024-11-13 16:31:23 -0300

    feat: O que são condicionais e booleanos?

--- 01-fundamentos/index.php
+++ 01-fundamentos/index.php
@@ -11,10 +11,42 @@
     $saudacao = "Oi, ";
     $titulo = $saudacao . "Portifolio do " . $nome;
     $subtitulo = "Seja bem vindo ao meu portifolio!!!";
-    $ano = 2024;
+    $ano = 2020;
+
+    $projeto = "Meu Portifolio";
+    $finalizado = true; // true,1 ou false,0
+    $dataDoProjeto = "2024-11-13";
+    $descricao = "Meu primeiro portifolio. Escrito em PHP e HTML.";
   ?>
   <h1><?=$titulo; ?></h1>
   <p><?=$subtitulo; ?></p>
   <p><?php echo $ano; ?></p>
+
+  <hr>
+  <div
+    <?php if((2024 - $ano) > 2): ?>
+      style="background-color: burlywood;"
+    <?php endif;?>
+  >
+    <h2><?=$projeto;?></h2>
+    <p><?=$descricao;?></p>
+    <div>
+      <div><?=$dataDoProjeto;?></div>
+      <div>Projeto:
+        <?php if(!$finalizado): ?>
+          <span style="color: red;">Não finalizado</span>
+        <?php else: ?>
+          <span style="color: green;">Finalizado</span>
+        <?php endif; ?>
+        <?php
+          if($finalizado) {
+            echo "Finalizado";
+          } else {
+            echo "Não finalizado";
+          }
+        ?>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
