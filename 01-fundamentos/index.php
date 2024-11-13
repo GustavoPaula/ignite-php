b8d9b0dc77a28010b78e71bc8976dd6581cd5b06
GustavoPaula <gustavo.sdepaula09@gmail.com>
2024-11-13 15:41:21 -0300

    feat: Primeira tag PHP

---
+++ 01-fundamentos/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Meu Portifolio</title>
+</head>
+<body>
+  <h1>
+    <?php 
+      echo "Portifolio do Gustavo";
+    ?>
+  </h1>
+  <p>
+    <?php
+      echo "TAG PHP"
+    ?>
+  </p>
+</body>
+</html>
