2d5702f7054cee8f625ce9ca4ec573137c40231b
GustavoPaula <gustavo.sdepaula09@gmail.com>
2024-11-13 18:00:13 -0300

    feat: Entendendo sobre arrays

--- 01-fundamentos/index.php
+++ 01-fundamentos/index.php
@@ -17,36 +17,49 @@
     $finalizado = true; // true,1 ou false,0
     $dataDoProjeto = "2024-11-13";
     $descricao = "Meu primeiro portifolio. Escrito em PHP e HTML.";
+
+    $projetos = [
+      [
+        "titulo" => "Meu portifolio",
+        "finalizado" => false,
+        "data" => "2024-11-13",
+        "descricao" => "Meu primeiro portifolio. Escrito em PHP e HTML."
+      ],
+      [
+        "titulo" => "Lista de tarefas",
+        "finalizado" => true,
+        "data" => "2024-11-11",
+        "descricao" => "Lista de tarefas. Escrito em PHP e HTML."
+      ]
+    ]
   ?>
   <h1><?=$titulo; ?></h1>
   <p><?=$subtitulo; ?></p>
   <p><?php echo $ano; ?></p>
 
   <hr>
-  <div
+
+  <ul>
+    <?php foreach ($projetos as $projeto): ?>
+      <div
     <?php if((2024 - $ano) > 2): ?>
       style="background-color: burlywood;"
     <?php endif;?>
   >
-    <h2><?=$projeto;?></h2>
-    <p><?=$descricao;?></p>
+    <h2><?=$projeto['titulo'];?></h2>
+    <p><?=$projeto['descricao'];?></p>
     <div>
-      <div><?=$dataDoProjeto;?></div>
+      <div><?=$projeto['data'];?></div>
       <div>Projeto:
-        <?php if(!$finalizado): ?>
+        <?php if(!$projeto['finalizado']): ?>
           <span style="color: red;">Não finalizado</span>
         <?php else: ?>
           <span style="color: green;">Finalizado</span>
         <?php endif; ?>
-        <?php
-          if($finalizado) {
-            echo "Finalizado";
-          } else {
-            echo "Não finalizado";
-          }
-        ?>
       </div>
     </div>
   </div>
+    <?php endforeach; ?>
+  </ul>
 </body>
 </html>
