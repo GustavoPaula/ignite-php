8fe6d76c06f52c9543d9adaa7b530c1f018bca4a
GustavoPaula <gustavo.sdepaula09@gmail.com>
2024-11-21 12:05:31 -0300

    feat: Criando nosso login com Laravel

--- 04-fundamentos-laravel/biolinks/app/Http/Controllers/auth/LoginController.php
+++ 04-fundamentos-laravel/biolinks/app/Http/Controllers/auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -14,6 +16,18 @@ class LoginController extends Controller
 
     public function login()
     {
-        dd(request()->all());
+        if($user = User::query()
+            ->where('email', '=', request()->email)
+            ->first()) 
+            {
+
+                if(Hash::check(request()->password, $user->password))
+                {
+                    auth()->login($user);
+                    return to_route('dashboard');
+                };
+            };
+
+        return back()->with(['messagem'=>'Não encontrado']);
     }
 }
--- 04-fundamentos-laravel/biolinks/resources/views/auth/login.blade.php
+++ 04-fundamentos-laravel/biolinks/resources/views/auth/login.blade.php
@@ -1,7 +1,10 @@
 <div>
     <h1>Login</h1>
+    <form action="/login" method="post">
+        @csrf
+        <input name="email" placeholder="Email" value="{{ old('email') }}"/>
+        <input type="password" name="password" placeholder="Senha"/>
 
-    <div>
-        
-    </div>
+        <button>Logar</button>
+    </form>
 </div>
